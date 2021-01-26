98d56a52e9ff0c880fff251b5cd5bfd5d34f4f7b
Donovan Baarda <abo@minkirri.apana.org.au>
2021-01-26 12:57:06 +1100

    Improve Chunker API and stats.
    Simplify Data and remove block counting.
    
    Add blkstats and dupstats collection to Chunker. Split getblock() to
    isblock() and addblock() methods, where isblock checks rollsums for block
    boundaries and addblock() does duplicate detection and updates stats. Improve
    __str__() output to show dupstats and wrap over multiple lines.
    
    For WiebullChunker remove obsolete comments and tidy code.
    
    For FastWeibull2Chunker ensure self.incr is an int.
    
    Simplify runtest() to take advantage of new Chunker stats collection.
    
    Remove irrelevant graph comments at end.

--- chunker.py
+++ chunker.py
@@ -48,7 +48,7 @@ class Data(object):
   getroll(). A simulated strong hash of the previous block can be fetched with
   gethash(), which also starts a new block.
 
-  It keeps counts of the number of bytes, duplicate bytes, and blocks fetched.
+  It keeps counts of the number of bytes and duplicate bytes.
   """
 
   def __init__(self, bsize=1024, bnum=512, mnum=5, seed=1):
@@ -67,7 +67,6 @@ class Data(object):
   def reset(self):
     self.tot_c = 0         # total bytes scanned.
     self.dup_c = 0         # duplicate bytes scanned.
-    self.blk_n = 0         # number of block hashes returned.
     self.blkh = 0          # the accumulated whole block hash.
     # Initialize the random generators for the original and inserted data.
     self.dat = random.Random(self.seed)
@@ -101,24 +100,20 @@ class Data(object):
         h = self.dat.randrange(2**32)
     self.tot_c += 1
     # update blkh.
-    self.addhash(h)
+    self.blkh = hash((self.blkh, h))
     return h
 
-  def addhash(self, h):
-    self.blkh = hash((self.blkh, h))
-
-  def gethash(self, l):
+  def gethash(self):
     """ Get a strong hash of the past l bytes and reset for a new block. """
     blkh, self.blkh = self.blkh, 0
-    self.blk_n += 1
     return blkh
 
   def __repr__(self):
     return "Data(bsize=%s, bnum=%s, mnum=%s, seed=%s)" % (self.bsize, self.bnum, self.mnum, self.seed)
 
   def __str__(self):
-    return "%r: tot=%d dup=%d(%4.1f%%), blk=%d" % (
-        self, self.tot_c, self.dup_c, 100.0 * self.dup_c / self.tot_c, self.blk_n)
+    return "%r: tot=%d dup=%d(%4.1f%%)" % (
+        self, self.tot_c, self.dup_c, 100.0 * self.dup_c / self.tot_c)
 
 
 class Chunker(object):
@@ -163,7 +158,9 @@ class Chunker(object):
     return solve(lambda x: cls.get_avg_len(x, min_len, max_len) - avg_len, 0.0)
 
   def reset(self):
-    self.stats = Sample()
+    self.blocks = {}
+    self.blkstats = Sample()
+    self.dupstats = Sample()
     self.initblock()
 
   def initblock(self):
@@ -173,21 +170,26 @@ class Chunker(object):
   def incblock(self):
     self.blk_len += 1
 
-  def getblock(self, h):
-    """ Returns the block length or 0 if not a break point. """
+  def isblock(self, r):
+    """ Checks if rollsum r is a break point and increments the block. """
     self.incblock()
-    if self.blk_len >= self.min_len and (h < self.prob or self.blk_len == self.max_len):
-      l = self.blk_len
-      self.stats.add(l)
-      self.initblock()
-      return l
-    return 0
+    return self.blk_len >= self.min_len and (r < self.prob or self.blk_len >= self.max_len)
+
+  def addblock(self, h):
+    """ Adds a block with hash h and initializes for the next block. """
+    l = self.blk_len
+    b = (h, l)
+    n = self.blocks[b] = self.blocks.setdefault(b, 0) + 1
+    self.blkstats.add(l)
+    if n > 1:
+      self.dupstats.add(l)
+    self.initblock()
 
   def __repr__(self):
     return "%s(tgt_len=%s, min_len=%s, max_len=%s)" % (self.__class__.__name__, self.tgt_len, self.min_len, self.max_len)
 
   def __str__(self):
-    return "%r: avg_len=%s %s" % (self, self.avg_len, self.stats)
+    return "%r: avg_len=%s\n  blks: %s\n  dups: %s" % (self, self.avg_len, self.blkstats, self.dupstats)
 
 
 class WeibullChunker(Chunker):
@@ -236,28 +238,21 @@ class WeibullChunker(Chunker):
     return min_len + L * gammalower(s, z) + t*e**(-z)
 
   def reset(self):
+    super(WeibullChunker, self).reset()
+    # Set the M probability multiplier.
     k = self.P + 1
     L = self.tgt_len / gamma(1.0 + 1.0/k)
     self.M = 2**32 * k / L**k
-    # step and incr are an incremental way to calculate p = K * x^2. The initial
-    # step for incrementing p is p calculated for x=1, and at each update we
-    # increment step by 2x that much. We calculate the increment here since it
-    # is fixed, and reset step to half that in initblock().
-    #self.incr = 2**32 * 4.0 / self.tgt_len**3
-    super(WeibullChunker, self).reset()
 
   def initblock(self):
     self.blk_len = 0
     self.prob = 0.0
-    #self.step = self.incr / 2.0
 
   def incblock(self):
     self.blk_len += 1
     x = self.blk_len - self.min_len
     if x > 0:
       self.prob = int(self.M * x**self.P)
-      #self.prob += self.step
-      #self.step += self.incr
 
 class Weibull1Chunker(WeibullChunker):
   P = 1
@@ -323,7 +318,7 @@ class FastWeibull2Chunker(Weibull2Chunker):
     while incr < 128:
       dx *= 2
       incr = 2 * self.M * dx**2
-    self.incr, self.mask = incr, dx - 1
+    self.incr, self.mask = int(incr + 0.5), dx - 1
 
   def initblock(self):
     self.blk_len = 0
@@ -340,30 +335,21 @@ class FastWeibull2Chunker(Weibull2Chunker):
 
 def runtest(data, chunker, data_len):
   blocks = {}
-  l = 0
   # Stop after we've read enough data and finished a whole block.
-  while data.tot_c < data_len or l == 0:
-    h = data.getroll()
-    l = chunker.getblock(h)
-    if l:
-      b = (data.gethash(l), l)
-      blocks[b] = blocks.setdefault(b, 0) + 1
-      #print "%12d: %08x %016x len=%d dup=%s" % (data.tot_c, h, b[0], b[1], blocks[b]-1)
-  # get stats on duplicate blocs.
-  tot_c = tot_n = 0
-  dup_c = dup_n = 0
-  for (h,l), n in blocks.items():
-    tot_n += n
-    tot_c += n * l
-    dup_n += (n - 1)
-    dup_c += (n - 1) * l
+  while data.tot_c < data_len or chunker.blk_len:
+    if chunker.isblock(data.getroll()):
+      chunker.addblock(data.gethash())
   print data
   print chunker
+  assert data.tot_c == chunker.blkstats.sum
+  tot_n, dup_n = chunker.blkstats.num, chunker.dupstats.num
+  tot_c, dup_c = chunker.blkstats.sum, chunker.dupstats.sum
+  perf = float(dup_c) / data.dup_c
   print "bytes: tot=%s dup=%s(%4.2f%%)" % ( tot_c, dup_c, 100.0 * dup_c / tot_c)
   print "blocks: tot=%s dup=%s(%4.2f%%)" % ( tot_n, dup_n, 100.0 * dup_n / tot_n)
-  print "found: %4.2f%%" % (100.0 * dup_c / data.dup_c)
+  print "found: %4.2f%%" % (100.0 * perf)
   print
-  return tot_n, tot_c, dup_n, dup_c
+  return data.tot_c, data.dup_c, chunker.blkstats, chunker.dupstats, perf
 
 
 tsize=2*1000
@@ -374,16 +360,7 @@ for bavg in (1,2,4,8,16,32,64):
   for bmin in (0, 1, 2, 3):
     bmin *= bavg / 4
     for bmax in (16, 8, 4, 2):
-      bmax = bmax*bavg
+      bmax *= bavg
       data.reset()
-      chunker = WeibullChunker.from_avg(bavg, bmin, bmax)
+      chunker = Weibull2Chunker.from_avg(bavg, bmin, bmax)
       runtest(data, chunker, tsize*bsize)
-
-# Dimensions for graphs;
-#
-# chunker (chunker, normchunker, FastCDCChunker)
-# min size (0, 1/4, 1/2, 3/4)
-# max_size (16, 8, 4, 2)
-# avg_size (1,2,4, 8, 16, 32, 64)
-#
-#
